e7b4e31715227b53f4372d8c76e567922cb34a84
AilsyaRachma03 <63080602+AilsyaRachma03@users.noreply.github.com>
2021-05-15 20:34:43 +0700

    Quiz2

--- home.php
+++ home.php
@@ -14,6 +14,7 @@ if(! isset($_SESSION['is_login']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Beranda | <?php echo $_SESSION['nama']; ?>  </title>
     <!-- <meta name="description" content=""> -->
   </head>
 
